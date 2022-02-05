703a8bdb0993f1134890482c4216d6046e6b1f0e
NikBel3476 <nikita.belkin3476@gmail.com>
2022-02-05 20:48:16 +0400

    added user authorization

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
 const UserController = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = new Router();
 
-router.get('/auth', UserController.check);
+router.get('/auth', authMiddleware, UserController.check);
 router.post('/login', UserController.login);
 router.post('/registration', UserController.registration);
 
