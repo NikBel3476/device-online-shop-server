465493c383ff03b1df81d6593538936020e68e9f
NikBel3476 <nikita.belkin3476@gmail.com>
2022-02-09 16:21:20 +0400

    added index.html

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
 app.use('/api', router);
+app.use('/', express.static(path.resolve(__dirname, 'static', 'index.html')));
 app.use(errorHandler);
 
 
